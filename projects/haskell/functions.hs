11542014597c66835dea73be02b183877d02aeba
João Barreto <Joo.bcsilva@upe.br>
2026-03-12 17:52:55 -0300

    Atualizar "functions" que contém algumas funções básicas.

--- projects/haskell/functions.hs
+++ projects/haskell/functions.hs
@@ -1,11 +1,20 @@
+import Text.Printf
+
 somar :: Int -> Int -> Int
 somar x y = x + y
 
 power :: Num a => a -> a
 power x = x * x
 
-pctInt :: Num a => a -> a -> a
-pctInt x y = (fromIntegral x / fromIntegral y) * 100
+calcPct :: Integral a => a -> a -> Double
+calcPct x y = (fromIntegral x / fromIntegral y) * 100
 
-pctFl :: a
+pctShow :: Integral a => a -> a -> IO ()
+pctShow x y = printf "Result: %.1f%%\n" (calcPct x y)
 
+somarL :: Num a => [a] -> a
+somarL [] = 0
+somarL (x:xs) = x + somarL xs
+
+juntar :: String -> String -> String
+juntar sx sy = sx ++ sy
