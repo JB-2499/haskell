a4ae66fb7bde223bb91dfff38ef7e22fac4b3314
João Barreto <Joo.bcsilva@upe.br>
2026-03-18 00:13:51 -0300

    Atualizar "functions2.hs".

--- projects/haskell/functions/functions2.hs
+++ projects/haskell/functions/functions2.hs
@@ -1,4 +1,21 @@
+merr :: String
+merr = "Error! Type not supported by this function."
+
 tamanho :: [a] -> Int
-tamanho xs | xs == [a] = sum [1 | _ <- xs]
-           | xs == [] = 0
-           | otherwise = error "Error! Type not suported by this function."
+tamanho [] = 0
+tamanho xs = sum [1 | _ <- xs] 
+
+suc :: Num a => a -> a
+suc x = x + 1
+
+seqnum :: Show a => [a] -> IO ()
+seqnum [] = return ()
+seqnum (x:xs) = do
+    putStrLn (show x ++ ",")
+    seqnum xs
+
+dlower :: String -> String
+dlower x = [c | c <- x, elem c ['A' .. 'Z']]
+
+dupper :: String -> String
+dupper x = [c | c <- x, elem c ['a' .. 'z']]
